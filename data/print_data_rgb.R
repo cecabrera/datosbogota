0f109bc04a0dbb0e53549c8f6e6d19483e0cb8bc
cecabrera <cecabrera55@gmail.com>
2017-05-21 12:33:18 -0500

    Actualiazación

--- data/print_data_rgb.R
+++ data/print_data_rgb.R
@@ -3,4 +3,6 @@ d <- fread("clean_data_rgb.csv")
 write.table(
   paste0("tramo", d$V1, ":{center:{", d$LATITUD_DESTINO,", ", d$LONGITUD_DESTINO, "}, color:'", d$RGB, "'},")
   , "clean_data_rgb_js.csv"
+  , row.names = FALSE
+  , sep = ","
 )
