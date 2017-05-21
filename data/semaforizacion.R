9d2585db2eb367dec2076a46309388f02da1fee1
cecabrera <cecabrera55@gmail.com>
2017-05-21 10:01:23 -0500

    correción de coordenadas

--- data/semaforizacion.R
+++ data/semaforizacion.R
@@ -1,6 +1,6 @@
 require(data.table)
 d <- fread("data/Semaforizaci_n_en_Bogota.csv")
-write.table(x = paste("new google.maps.LatLng(", d$x, ",", d$y, "),", sep = "")
+write.table(x = paste("new google.maps.LatLng(", d$y, ",", d$x, "),", sep = "")
             , file = "semaforos.csv"
             , row.names = FALSE
             , sep = ","
