a21fa903f38014a81fd9a0b5f0c2905a3a941910
cecabrera <cecabrera55@gmail.com>
2017-05-21 12:30:55 -0500

    Impresión del RGB para Javascript

---
+++ data/print_data_rgb.R
@@ -0,0 +1,6 @@
+require(data.table)
+d <- fread("clean_data_rgb.csv")
+write.table(
+  paste0("tramo", d$V1, ":{center:{", d$LATITUD_DESTINO,", ", d$LONGITUD_DESTINO, "}, color:'", d$RGB, "'},")
+  , "clean_data_rgb_js.csv"
+)
